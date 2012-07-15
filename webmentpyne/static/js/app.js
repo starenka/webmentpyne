abe16eb81a015c08f5d50d600d5292a71af5e9b4
starenka <starenka0@gmail.com>
2012-07-15 13:09:33 +0200

    better status bar, refreshing, more fixes

--- webmentpyne/static/js/app.js
+++ webmentpyne/static/js/app.js
@@ -1,28 +1,32 @@
-(function worker() {
+function get_status() {
     $.ajax({url:'/player/',
         success:function (data, textStatus, jqXHR) {
             if (data.status) {
                 $('#controls').show();
                 $('#current').html(data.response.html);
+                $('#status').html(data.response.PlaybackStatus.toLowerCase());
+                $('#shuffle').html('shuffle: ' + (data.response.Shuffle?'yes':'no'));
+                $('#repeat').html('repeat: ' + data.response.LoopStatus.toLowerCase());
+                $('#volume').html('volume: '+ data.response.Volume*100 + '%');
             }
             else {
                 $('#current').load('connection-error');
                 $('#controls').hide();
             }
-        },
-        complete:function () {
-            setTimeout(worker, 5000);
         }})
-})();
-
+}
 
 $(document).ready(function () {
     $('#controls button').click(function () {
         $.get('/player/' + this.id,function (data, textStatus, jqXHR) {
+            get_status();
         }).error(function () {
                 alert('It seems that Clementine or webserver is not running.')
             });
     });
+
+    get_status();
+    setInterval(get_status, 5000);
 
 });
 
